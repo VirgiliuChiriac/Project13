a1ce7822b94ee19579703d519880f0e7d83ae838
bivoldaniel <bivol.daniel09@yahoo.com>
2017-06-13 00:30:42 +0300

    added

---
+++ init3.sql
@@ -0,0 +1,131 @@
+declare
+  v_id  integer := 1;
+  v_id_user integer;
+  v_tip varchar2(50);
+  v_achizitie varchar2(50);
+  v_pret integer;
+  v_suprf_utila integer;
+begin
+  while (v_id <= 3500000) loop
+      v_id_user := trunc(dbms_random.value(1,100));
+      v_pret := trunc(dbms_random.value(30000,100000));
+      v_suprf_utila := trunc(dbms_random.value(30,180));
+      
+      if(v_id < 1500000) then
+        v_tip := 'Apartament/Garsoniera';
+      elsif(v_id >= 1500000 and v_id < 2500000) then
+        v_tip := 'Casa/Vila';
+      elsif(v_id >= 2500000 and v_id < 3000000) then
+        v_tip := 'Teren de constructii';
+      elsif(v_id >=3000000 and v_id <= 3500000) then
+        v_tip := 'Teren agricol';
+      end if;
+      
+      if(v_id < 750000) then
+        v_achizitie := 'Inchiriere';
+      elsif(v_id >= 750000 and v_id < 2000000) then
+        v_achizitie := 'Vanzare';
+      elsif(v_id >= 2000000 and v_id < 2500000) then
+        v_achizitie := 'Inchiriere';
+      elsif(v_id >= 2500000  and v_id < 2750000) then
+        v_achizitie := 'Vanzare';
+      elsif(v_id >= 2750000  and v_id < 3250000) then
+        v_achizitie := 'Inchiriere';
+      elsif(v_id >= 3250000 and v_id <= 3500000) then
+        v_achizitie := 'Vanzare';
+      end if; 
+      
+      INSERT INTO InfoProdus(id_produs, id_user, tip, achizitie, pret, suprf_utila) values (v_id, v_id_user, v_tip, v_achizitie, v_pret, v_suprf_utila);
+      v_id := v_id +1;
+  end loop;
+commit;
+end; 
+/
+declare
+  v_id_extra integer;
+  v_id_produs integer;
+  v_nr_camere integer;
+  v_nr_bai integer;
+  v_descriere VARCHAR2(1000);
+  v_confort integer;
+  v_vechime integer;
+  v_niveluri_imobil integer;
+  v_etaj integer;
+  v_tip_teren VARCHAR2(20);
+  v_clasificare VARCHAR2(20);
+  v_coordonata_lat VARCHAR2(20);
+  v_coordonata_long VARCHAR2(20);
+begin
+  v_id_extra := 1;
+  v_id_produs := 1;
+  
+  while (v_id_extra <= 3500000) loop
+      v_descriere := DBMS_RANDOM.STRING('L', 20);
+      
+      if(v_id_extra < 2500000) then
+        v_nr_camere := trunc(dbms_random.value(1,6));
+        v_nr_bai := trunc(dbms_random.value(1,3));
+        v_confort := trunc(dbms_random.value(1,5));
+        v_vechime := trunc(dbms_random.value(1,30));
+        v_tip_teren := null;
+        v_niveluri_imobil := null;
+        v_etaj := null;
+      end if;
+      
+      if(v_id_extra <= 1500000) then
+        v_niveluri_imobil := trunc(dbms_random.value(0,30));
+        v_etaj := trunc(dbms_random.value(0,30));
+      end if;
+      
+      if(v_id_extra >= 2500000 and v_id_extra < 2750000) then
+        v_nr_camere := null;
+        v_nr_bai := null;
+        v_confort := null;
+        v_vechime := null;
+        v_tip_teren := null;
+        v_clasificare := 'Intravilan';
+        v_niveluri_imobil := null;
+        v_etaj := null;
+      end if;
+      
+      if(v_id_extra >= 27500000 and v_id_extra < 3000000) then
+        v_nr_camere := null;
+        v_nr_bai := null;
+        v_confort := null;
+        v_vechime := null;
+        v_tip_teren := null;
+        v_clasificare := 'Extravilan';
+        v_niveluri_imobil := null;
+        v_etaj := null;
+      end if;
+      
+      if(v_id_extra >= 3000000 and v_id_extra < 3250000) then
+        v_nr_camere := null;
+        v_nr_bai := null;
+        v_confort := null;
+        v_vechime := null;
+        v_clasificare := 'Extravilan';
+        v_tip_teren := 'Livada';
+        v_niveluri_imobil := null;
+        v_etaj := null;
+      end if;
+      
+      if(v_id_extra >= 3250000and v_id_extra <= 3500000) then
+        v_nr_camere := null;
+        v_nr_bai := null;
+        v_confort := null;
+        v_vechime := null;
+        v_clasificare := 'Intravilan';
+        v_tip_teren := 'Teren Arabil';
+        v_niveluri_imobil := null;
+        v_etaj := null;
+      end if;      
+      
+      
+      INSERT INTO ExtraInfoProdus(id_extra, id_produs, nr_camere, nr_bai, descriere, confort, vechime, niveluri_imobil, etaj, tip_teren) values (v_id_extra, v_id_produs, v_nr_camere, v_nr_bai, v_descriere, v_confort, v_vechime, v_niveluri_imobil, v_etaj, v_tip_teren);
+      v_id_extra := v_id_extra + 1;
+      v_id_produs := v_id_produs + 1;
+  end loop;
+commit;
+end; 
+/
--- view/scripts/menu.js
+++ view/scripts/menu.js
@@ -1,35 +1,5 @@
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 var loggedIn = 0;
-
 function logedMenu(){
-    if(getCookie("user_id")!=""){
-        loggedIn = 1;
-        document.getElementById("logBox").style.display="none";
-        document.getElementById("close").style.display="none";
-    } else {
-        loggedIn = 0;
-    }
-
     if(loggedIn==0){
         document.getElementById("logged0").style.display="inline-block";
         document.getElementById("logged1").style.display="none";
@@ -43,137 +13,32 @@ function logedMenu(){
     }
 }
 
-var address = "http://ec2-35-165-78-223.us-west-2.compute.amazonaws.com/";
-
-function LogIn(){
-    $.post(address + "login",{
-        user: document.getElementById('in-email').value,
-        pass: document.getElementById('in-pass').value
-    }, function(data){
-        if(data[0]!=null){
-            document.getElementById('in-email').value="";
-            document.getElementById('in-pass').value="";
-            setCookie("user_id",data[0],1);
-            logedMenu();
-        } else {
-            setCookie("user_id","",-1);
-            logedMenu();
-        }
-    });
+function toggleLoggedIn(){
+    if(loggedIn==0)
+        loggedIn = 1;
+    else 
+        loggedIn = 0;
+    logedMenu();
 }
 logedMenu();
 
-function toggleLoggedIn(){
-    FB.logout(function(response) {
-        console.log(response);
-    });
-    setCookie("user_id","",-1);
-    logedMenu();
-}
-
 document.getElementById("logBox").style.display="none";
-document.getElementById("close").style.display="none";
 document.getElementById("signUpBox").style.display="none";
 
 $("#logged0").click(function(){
     $("#logBox").toggle();
-    $("#close").toggle();
     document.getElementById("logInBox").style.display="block";
     document.getElementById("signUpBox").style.display="none";
 });
-
-$("#close").click(function(){
-    $("#logBox").toggle();
-    $("#close").toggle();
-});
-
 $("#signUpBx").click(function(){
     document.getElementById("logInBox").style.display="none";
     document.getElementById("signUpBox").style.display="block";
 });
-
 $("#logInBx").click(function(){
     document.getElementById("logInBox").style.display="block";
     document.getElementById("signUpBox").style.display="none";
 });
 
-$("#signUpB").click(function(){
-    if(document.getElementById('up-email').value == "" || document.getElementById('up-pass').value == ""){
-        alert("Email and Password can't be null");
-    } else {
-
-        $.post(address + "signUp",{
-            fname: document.getElementById('up-fname').value == "" ? "-" : document.getElementById('up-fname').value,
-            lnane: document.getElementById('up-lname').value == "" ? "-" : document.getElementById('up-lname').value,
-            user: document.getElementById('up-email').value == "" ? "-" : document.getElementById('up-email').value,
-            pass: document.getElementById('up-pass').value == "" ? "-" : document.getElementById('up-pass').value,
-            phone: document.getElementById('up-phone').value == "" ? "-" : document.getElementById('up-phone').value
-        }, function(data){
-            if(data[0]!=null){
-                document.getElementById('up-fname').value="";
-                document.getElementById('up-lname').value="";
-                document.getElementById('up-email').value="";
-                document.getElementById('up-pass').value="";
-                document.getElementById('up-phone').value="";
-                setCookie("user_id",data[0],1);
-                logedMenu();
-            } else {
-                setCookie("user_id","",-1);
-                logedMenu();
-            }
-        });
-    }
+$(".whiteBG").click(function(){
+    document.getElementById("logBox").style.display="none";
 });
-
-$(document).on(
-    'fbload',  //  <---- HERE'S OUR CUSTOM EVENT BEING LISTENED FOR
-    function(){
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-
-    }
-);
-
-function statusChangeCallback(response){
-    console.log("this function" + response);
-    if(response.status!='unknown'){
-        console.log("in the log-in funcion");
-        $.post(address + "login",{
-            user: response.authResponse.userID.toString(),
-            pass: "-"
-        }, function(data){
-            if(data[0]!=null){
-                setCookie("user_id",data[0],1);
-                logedMenu();
-                console.log("logged fb");
-            } else {
-                $.post(address + "signUp",{
-                    fname: "-",
-                    lnane: "-",
-                    user: response.authResponse.userID.toString(),
-                    pass: "-",
-                    phone: "-"
-                }, function(data){
-                    if(data[0]!=null){
-                        console.log("sign up fb");
-                        setCookie("user_id",data[0],1);
-                        logedMenu();
-                    } else {
-                        console.log("error fb");
-                        setCookie("user_id","",-1);
-                        logedMenu();
-                    }
-                });
-            }
-        });
-    } else {
-        console.log("unlogged fb");
-    }
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
